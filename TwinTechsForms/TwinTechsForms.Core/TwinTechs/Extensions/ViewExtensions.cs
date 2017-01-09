cf948fdef1d3ab97a5e047295335e0765bba91db
Tom Soderling <Tommy@Toms-MBP-2.hsd1.mn.comcast.net>
2017-01-09 13:59:04 -0600

    updated formatting, addd comment

--- TwinTechsForms/TwinTechsForms.Core/TwinTechs/Extensions/ViewExtensions.cs
+++ TwinTechsForms/TwinTechsForms.Core/TwinTechs/Extensions/ViewExtensions.cs
@@ -10,17 +10,20 @@ namespace TwinTechs.Extensions
 		/// </summary>
 		/// <returns>The page.</returns>
 		/// <param name="element">Element.</param>
-		public static Page GetParentPage (this VisualElement element)
+		public static Page GetParentPage(this VisualElement element)
 		{
-			if (element != null) {
+			if (element != null)
+			{
 				var parent = element.Parent;
-				while (parent != null) {
-					if (parent is Page) {
+				while (parent != null)
+				{
+					// keep moving up the visual tree until a Page is found
+					if (parent is Page)
 						return parent as Page;
-					}
 					parent = parent.Parent;
 				}
 			}
+
 			return null;
 		}
 	}
