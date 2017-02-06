04ab997dedd04aa75f101d4a64cc43b153ffa542
Tom Soderling <tom.soderling@mcomposer.com>
2017-02-06 12:09:44 -0600

    Update to set the UWP.PageControl as the Content of the Control

--- TwinTechsForms/TwinTechsFormsExample/TwinTechsFormsExample.cs
+++ TwinTechsForms/TwinTechsFormsExample/TwinTechsFormsExample.cs
@@ -84,7 +84,7 @@ namespace TwinTechs
                 Content = _grid
             };
 
-            HandleBox1Tapped();
+            HandleBox1Tapped(); // start app displaying the navigation page for tab #1
 
             MainPage = _mainContentPage;
 
@@ -107,7 +107,10 @@ namespace TwinTechs
             {
                 _contentPage1 = new MyPage(0);
 
-                _currentNavigationPage = _navPage1 = new NavigationPage { Title = "Nav 1" };
+                _currentNavigationPage = _navPage1 = new NavigationPage
+                {
+                    Title = "Nav 1",
+                };
 
                 await _navPage1.PushAsync(_contentPage1);
             }
@@ -133,7 +136,12 @@ namespace TwinTechs
             if (_navPage2 == null)
             {
                 _contentPage2 = new MyPage(0);
-                _currentNavigationPage = _navPage2 = new NavigationPage { Title = "Nav 2" };
+
+                _currentNavigationPage = _navPage2 = new NavigationPage
+                {
+                    Title = "Nav 2",
+                };
+
                 await _navPage2.PushAsync(_contentPage2);
             }
             else
